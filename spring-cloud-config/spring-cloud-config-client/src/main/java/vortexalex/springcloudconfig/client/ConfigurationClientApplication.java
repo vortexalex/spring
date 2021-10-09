6945e0850bdd51061e276cc4ccb1e1c4d5d3a33a
a.cosenza <a.cosenza@almaviva.it>
2021-10-09 17:40:44 +0200

    add spring cloud config server and client applications

---
+++ spring-cloud-config/spring-cloud-config-client/src/main/java/vortexalex/springcloudconfig/client/ConfigurationClientApplication.java
@@ -0,0 +1,12 @@
+package vortexalex.springcloudconfig.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationClientApplication.class, args);
+    }
+}
