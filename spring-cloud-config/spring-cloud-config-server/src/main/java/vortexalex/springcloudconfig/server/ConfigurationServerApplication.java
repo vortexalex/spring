f3f9d6e0cd8594f716a660e8f91d84be17d06e19
a.cosenza <a.cosenza@almaviva.it>
2021-10-09 17:46:34 +0200

    rename class

--- spring-cloud-config/spring-cloud-config-server/src/main/java/vortexalex/springcloudconfig/server/ConfigurationServerApplication.java
+++ spring-cloud-config/spring-cloud-config-server/src/main/java/vortexalex/springcloudconfig/server/ConfigurationServerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class SpringCloudConfigServerApplication {
+public class ConfigurationServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+        SpringApplication.run(ConfigurationServerApplication.class, args);
     }
 }
